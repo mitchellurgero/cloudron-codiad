be7136ff3ba2a3b655c49c1159329e1447a488a0
Johannes Zellner <johannes@nebulon.de>
2016-05-30 23:58:19 +0200

    Enalbe ldap auth for webdav

--- start.sh
+++ start.sh
@@ -12,6 +12,11 @@ if [ ! -f "/app/data/apache2-app.conf" ]; then
     cp /app/code/apache2-app.conf /app/data/apache2-app.conf
 fi
 
+sed -e "s@AuthLDAPURL .*@AuthLDAPURL ${LDAP_URL}/${LDAP_USERS_BASE_DN}?username??(objectclass=user)@" \
+    -e "s@AuthLDAPBindDN .*@AuthLDAPBindDN ${LDAP_BIND_DN}@" \
+    -e "s@AuthLDAPBindPassword .*@AuthLDAPBindPassword ${LDAP_BIND_PASSWORD}@" \
+    -i /app/data/apache2-app.conf
+
 chown -R www-data:www-data /app/data /run/app
 
 echo "Starting apache"
