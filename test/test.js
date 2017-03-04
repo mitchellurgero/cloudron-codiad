75efd8f2d3bfb6bf941217c708131520695e4f20
Girish Ramakrishnan <girish@cloudron.io>
2017-03-03 20:30:07 -0800

    Fix test

--- test/test.js
+++ test/test.js
@@ -55,7 +55,7 @@ describe('Application life cycle test', function () {
         browser.get('https://' + app.fqdn);
 
         waitForElement(by.xpath('//*[text()="Cloudron LAMP App"]'), function () {
-            waitForElement(by.xpath('//*[text()="PHP Version 7.0.15-0ubuntu0.16.04.2"]'), callback);
+            waitForElement(by.xpath('//*[text()="PHP Version 7.0.15-0ubuntu0.16.04.4"]'), callback);
         });
     }
 
