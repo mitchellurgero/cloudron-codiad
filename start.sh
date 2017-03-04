4f077ec5469f2bec94a7fc347a942ff55cfbf036
Girish Ramakrishnan <girish@cloudron.io>
2017-03-03 20:03:39 -0800

    Fix display of sftp info

--- start.sh
+++ start.sh
@@ -35,8 +35,7 @@ else
         /app/code/proftpd.conf.template > /run/proftpd/proftpd.conf
 
     if [[ -f /app/data/public/index.php ]]; then
-        sed -e "s,##SFTP_PORT,${SFTP_PORT}," \
-            -e "s,##SFTP_DOMAIN,${APP_DOMAIN}," \
+        sed -e "s,^sftp -P.*public/$,sftp -P ${SFTP_PORT} ${APP_DOMAIN}:public/,"
             -i /app/data/public/index.php
     fi
 fi
