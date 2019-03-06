8041cb524fb417b7316f19d2f3c0cfaf46cb031c
Girish Ramakrishnan <girish@cloudron.io>
2019-03-05 16:12:32 -0800

    create session directory

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-mkdir -p /app/data/public /run/apache2 /run/proftpd /run/app /run/cron
+mkdir -p /app/data/public /run/apache2 /run/proftpd /run/cron /run/app/sessions
 
 # check if any index file exists
 for f in /app/data/public/index.*; do
